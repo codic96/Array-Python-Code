c1e0c53e9bd6159f096b7d936f0c9d92ea2b8a63
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2021-10-27 17:04:15 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,13 @@
+#Python 3 code to find sum
+#of elements in given array
+#driver function
+
+arr = []
+#input values to list
+arr = [12,3,4,15]
+#sum() is an inbuilt function in Python
+#the value
+ans = sum(arr)
+
+#display sum
+print('Sum of the array is',ans)
